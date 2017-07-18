b8de8f4a8a7352e1ee797078b9ca4ce3e758f63d
Mile Modic <me@meandkareem.com>
2017-07-18 11:37:40 +0200

    removed slideout
    Signed-off-by: Mile Modic <me@meandkareem.com>

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,16 @@
 import $ from "jquery";
-import slideout from 'slideout';
-
-var menu = new slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('menu'),
-    'padding': 256,
-    'tolerance': 70
-  });
-
-// Menu Toggle button
-document.getElementById('nav-toggle').addEventListener('click', function() {
-      menu.toggle();
-});
-
+// import slideout from 'slideout';
 //
+// var menu = new slideout({
+//     'panel': document.getElementById('panel'),
+//     'menu': document.getElementById('menu'),
+//     'padding': 256,
+//     'tolerance': 70
+//   });
+//
+// // Menu Toggle button
+// document.getElementById('nav-toggle').addEventListener('click', function() {
+//       menu.toggle();
+// });
+//
+// //
